dc88e8c9d30c4e515874bc68892a718f88763c5d
Solaiman Hossain <slmnsnto@gmail.com>
2025-06-24 11:54:20 +0600

    change middleware

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://luna3server.onrender.com/api/v1", "http://127.0.0.1:8000", "http://localhost:5000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
